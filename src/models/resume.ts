0713f77807e6d1621b6585a359dbaa8724da4bc6
angel-pup <vess.stewart@pm.me>
2023-07-12 20:24:36 -0500

    Updated styling for courses and updated resume information

--- src/models/resume.ts
+++ src/models/resume.ts
@@ -16,26 +16,20 @@ let resume: Resume = {
         github: "angel-pup",
     },
     summary: `
-    Results-driven full stack web developer with a keen eye for
-    detail and a passion for creating user-friendly and visually
-    appealing websites and applications. Equipped with excellent
-    communication, interpersonal, and creative thinking skills.
-    Seeking a challenging role where I can leverage my technical
-    expertise and innovative problem-solving abilities to contribute
-    to your company's success. Known for being a dependable team
-    player and a mentor, I am ready to apply my dedication and
-    continuous learning mindset to drive impactful solutions and
-    foster growth within your organization.
+    Full stack web developer with a passion for creating UX-driven
+    applications known for being a dependable team leader and mentor.
+    I am seeking a role where I can leverage my technical experience,
+    continuous learning mindset, and contribute to your organization's goals.
     `.replace(regex, ' '),
     skills: [
         "MERN Stack",
-        "JIRA",
         "SQL",
         "Firebase",
         "REST",
-        "TypeScript",
         "GraphQL",
-        "Heroku"
+        "TypeScript",
+        "JIRA",
+        "Git"
     ],
     certifications: [
         "ITIL Foundation Certificate in IT Service Management",
@@ -50,9 +44,9 @@ let resume: Resume = {
                 null
             ).format(),
             description: [
-                "Successfully resolved critical priority one issues promptly, demonstrating strong problem-solving skills and the ability to work under pressure.",
-                "Collaborated effectively with cross-functional teams to troubleshoot and escalate complex issues, fostering a collaborative environment focused on issue resolution and customer satisfaction.",
-                "Contributed to documenting and troubleshooting issues across a multitude of software and services for a diverse portfolio of client companies.",
+                "Successfully resolved critical issues promptly, demonstrating the ability to work under pressure.",
+                "Collaborated with cross-functional teams to troubleshoot and escalate complex issues.",
+                "Contributed to documentation for a multitude of software and services for a diverse portfolio of client companies.",
             ]
         },
         {
@@ -63,9 +57,9 @@ let resume: Resume = {
                 new Date(2022, 9, 1)
             ).format(),
             description: [
-                "Implemented and managed critical services, ensuring efficient performance and optimizing call routing systems.",
-                "Collaborated closely with cross-functional teams, including vendors and engineers, to successfully migrate CCX systems to a modern CCE/Calabrio environment",
-                "Developed intelligence reports that identified and resolved configuration issues to enhance system usability and functionality.",
+                "Implemented, managed and optimized high priority call routing systems, ensuring efficient performance.",
+                "Collaborated closely with vendors and engineers to successfully migrate CCX systems to a modern CCE/Calabrio environment",
+                "Developed intelligence reports that identified and resolved configuration issues enhancing system usability and functionality.",
             ]
         },
     ],
@@ -77,9 +71,21 @@ let resume: Resume = {
             ).format(),
             title: "Tech Blog",
             description: [
-                "Developed a simple MVC tech blog utilizing Express.js as the backend framework, Handlebars as the templating engine, and session management for user authentication. Implemented middleware functions for enhanced security and optimized routing. Leveraged MySQL database and Sequelize ORM for efficient data management and retrieval."
+                "Developed a MVC tech blog utilizing Express.js as the backend framework, Handlebars as the templating engine, and session management for user authentication. Implemented middleware functions for enhanced security and optimized routing. Leveraged MySQL database and Sequelize ORM for efficient data management and retrieval."
             ],
             repo: "angel-pup/tech-blog",
+            company: "KU Bootcamp"
+        },
+        {
+            date_range: new DateRange(
+                new Date(2023, 1),
+                new Date(2023, 2)
+            ).format(),
+            title: "Social Network API",
+            description: [
+                "Developed a RESTful API demonstration with mongoose ODM / MongoDB, hosted on Atlas with Node/Express server hosted on Heroku."
+            ],
+            repo: "angel-pup/social-network-api",
             company: "KU Bootcamp"
         },
     ],
@@ -103,7 +109,7 @@ let resume: Resume = {
             degree: "KU Full-Stack Flex Coding Boot Camp Certificate",
             relevant_coursework: [
                 "MVC",
-                "PWA",
+                "SQL & NoSQL",
                 "API",
                 "MERN",
                 "ORM",
